e55afa9fd9a31ac30a7a9d44ede4fa25244ce496
Alex Miller <alex.miller@cognitect.com>
2020-02-09 09:28:10 -0600

    add docs

---
+++ index-0.2.31-SNAPSHOT.clj
@@ -0,0 +1,7 @@
+{:namespaces
+ ({:doc nil,
+   :name "clojure.tools.deps.graph",
+   :wiki-url "https://clojure.github.io/tools.deps.graph/index.html",
+   :source-url
+   "https://github.com/clojure/tools.deps.graph/blob/e0ffcfa30c55c5af48034ba1cdae9771a51a7ce0/src/main/clojure/clojure/tools/deps/graph.clj"}),
+ :vars ()}
